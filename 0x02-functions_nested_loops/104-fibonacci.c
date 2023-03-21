7ff64a7099814a546e2c60f4eee2e802ba449331
ZAk-27 <touhadi.zakaria@gmail.com>
2023-03-21 05:28:46 -0700

    fibonacci 2

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+	/**
+	 * main - fibonacci <3
+	 *
+	 * Purpose - no hardcode
+	 *
+	 * Return:  (Success)
+	 */
+
+
+	int main(void)
+	{
+	
+
+
+
+
+		}
+		printf("\n");
+		return (0);
+	}
+
