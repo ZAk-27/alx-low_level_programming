2ab7169fe76d40514f0775e9e72400383970f671
ZAk-27 <touhadi.zakaria@gmail.com>
2023-03-23 17:46:57 -0700

    print lin

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,14 +1,34 @@
 #include "main.h"
 
 /**
- * print_line - print a straight line using putchar of n size
- * @n: size of line
+ * print_line - entry point
+ *	@n: int number
+ * Description: --
+ * Return: --
  */
-
 void print_line(int n)
 {
-for (; n > 0; n--)
-_putchar('_');
-_putchar('\n');
+	int i = 0;
+
+	while (i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
 }
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
 
