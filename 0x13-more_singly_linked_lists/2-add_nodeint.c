8c5dba9b42aa9529278f37537e5bca3be2ee5b2c
ZAk-27 <touhadi.zakaria@gmail.com>
2023-04-30 19:12:30 -0700

    nodeint

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint - adds a new node a  beginning of a listint_t list
+ * @head: first node of the linkd list
+ * @n: new value for new node
+ * Return: the address of the new element or NULL if it failed
+ *
+ * alx programm africa
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+
+	(*new).n = n;
+	(*new).next = *head;
+
+	*head = new;
+
+	return (*head);
+}
