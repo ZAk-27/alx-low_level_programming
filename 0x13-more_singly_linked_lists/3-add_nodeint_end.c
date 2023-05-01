48320daadddb5e731fd08ef6343bace715c65dac
ZAk-27 <touhadi.zakaria@gmail.com>
2023-04-30 19:16:38 -0700

    nodein-end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint_end - add node to the end
+ * @head: pointer to the begining of the linked list
+ * @n: value to add to node
+ * Return: the address of the new element  
+ *alx programm africa
+ *
+ */
+
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *last;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	(*new).n = n;
+	(*new).next = NULL;
+
+	if (head && !*head)
+	{
+		*head = new;
+		return (new);
+	}
+
+	for (last = *head; (*last).next; last = (*last).next)
+		;
+
+	(*last).next = new;
+
+	return (new);
+}
