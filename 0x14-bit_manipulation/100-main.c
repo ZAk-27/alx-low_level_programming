1bb0d3685974ddbf5ecadaccfd9173fe2b05a929
ZAk-27 <touhadi.zakaria@gmail.com>
2023-05-04 18:18:19 -0700

    adv task

--- 0x14-bit_manipulation/100-main.c
+++ 0x14-bit_manipulation/100-main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include "main.h"
 
-/**
- * get_endianness - checks if a machine is little or big endian
- * Return: 0 for big, 1 for little
- */
-
 int main(void)
 {
     int n;
