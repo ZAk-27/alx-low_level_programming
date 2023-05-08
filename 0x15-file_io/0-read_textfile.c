dd577b1afac17aaefb7cf20bf5a4b87c0504503b
ZAk-27 <touhadi.zakaria@gmail.com>
2023-05-08 15:00:36 -0700

    1st task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,33 +10,33 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 {
-	char *zak;
+	char *buf;
 	int of;
 	size_t lRead, check;
 
 	if (!filename || !letters)
 		return (0);
-	zak = malloc(letters);
-	if (!zak)
+	buf = malloc(letters);
+	if (!buf)
 		return (0);
 	/* open file */
 	of = open(filename, O_RDONLY);
 	if (of == -1)
 	{
-		free(zak)
+		free(buf)
 			return (0);
 	}
 	/* read file up to "letters" amount of char */
-	lRead = read(of, zak, letters);
+	lRead = read(of, buf, letters);
 	if (lRead < 1)
 	{
-		free(zak);
+		free(buf);
 		close(of);
 		return (0);
 	}
 	/* write content read */
-	check = write(STDOUT_FILENO, zak, lRead);
-	free(zak);
+	check = write(STDOUT_FILENO, buf, lRead);
+	free(buf);
 	close(of);
 
 	if (!check || check != lRead)
