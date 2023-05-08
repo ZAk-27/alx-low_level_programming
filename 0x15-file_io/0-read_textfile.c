ee92a3bc25588935bb36c9f4f34baf6f9a0c1860
ZAk-27 <touhadi.zakaria@gmail.com>
2023-05-08 15:18:02 -0700

    1st task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,33 +10,32 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 {
-	char *buf;
+	char *buff;
 	int of;
 	size_t lRead, check;
 
 	if (!filename || !letters)
 		return (0);
-	buf = malloc(letters);
-	if (!buf)
+
+	buff = malloc(letters);
+	if (!buff)
 		return (0);
-	/* open file */
 	of = open(filename, O_RDONLY);
 	if (of == -1)
 	{
-		free(buf)
-			return (0);
+		free(buff);
+		return (0);
 	}
-	/* read file up to "letters" amount of char */
-	lRead = read(of, buf, letters);
+	lRead = read(of, buff, letters);
 	if (lRead < 1)
 	{
-		free(buf);
+		free(buff);
 		close(of);
 		return (0);
 	}
-	/* write content read */
-	check = write(STDOUT_FILENO, buf, lRead);
-	free(buf);
+	check = write(STDOUT_FILENO, buff, lRead);
+
+	free(buff);
 	close(of);
 
 	if (!check || check != lRead)
