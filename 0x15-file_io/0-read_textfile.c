06ef6bf9c3afd374f2fa1fbceb9e842842e6b5c6
ZAk-27 <touhadi.zakaria@gmail.com>
2023-05-10 11:49:45 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,43 +4,24 @@
 /**
   * read_textfile - a txt file will be read nd printed to POSIX standard output
   * * @filename: file to read
-  * @letters: num of letters to be read nd printed 
+  * @letters: num of letters to be read nd printed
   * Return: num of letters to be  read and printd  0 if fails
   */
-
-ssize_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buff;
-	int of;
-	size_t lRead, check;
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (!filename || !letters)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	buff = malloc(letters);
-	if (!buff)
-		return (0);
-	of = open(filename, O_RDONLY);
-	if (of == -1)
-	{
-		free(buff);
-		return (0);
-	}
-	lRead = read(of, buff, letters);
-	if (lRead < 1)
-	{
-		free(buff);
-		close(of);
-		return (0);
-	}
-	check = write(STDOUT_FILENO, buff, lRead);
-
-	free(buff);
-	close(of);
-
-	if (!check || check != lRead)
-		return (0);
-
-	return (lRead);
+	free(buf);
+	close(fd);
+	return (w);
 }
-
