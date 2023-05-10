30f0a60732aec1f4b5221821c6324d7a5e78a4f0
ZAk-27 <touhadi.zakaria@gmail.com>
2023-05-10 12:24:55 -0700

    k

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,14 +12,23 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
-if (!filename)
-		return (-1);
-	fd = open(filename, O_RDWR | O_APPEND);
-	if (fd == -1)
-		return (-1);
-	while (text_content && text_content[len])
-		len++;
-	write(fd, text_content, len);
-	close(fd);
-	return (1);
 
+	if (!filename)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
